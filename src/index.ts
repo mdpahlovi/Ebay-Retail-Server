a7a8e773e81629f05c2b8f9094cba9efb88713e8
MD Pahlovi <mdpahlovi07@gmail.com>
2023-12-06 23:16:09 +0600

    [Done] Basic Project Setup

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { typeDefs } from "./graphql/schemas";
+import { resolvers } from "./graphql/resolvers";
+
+const bootstrap = async () => {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+    });
+
+    console.log(`🚀  Server ready at: ${url}`);
+};
+
+bootstrap();

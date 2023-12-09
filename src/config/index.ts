5b78ed9259a73469fd73607d16b1e90ef93724f0
MD Pahlovi <mdpahlovi07@gmail.com>
2023-12-09 22:25:16 +0600

    Rename JWT_SECRET

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,5 @@ require("dotenv").config({ path: path.join(process.cwd(), ".env") });
 export default {
     port: process.env.PORT,
     mongodb_url: process.env.MONGODB_URL,
-    jwt: { secret: process.env.JWT_SIGN },
+    jwt: { secret: process.env.JWT_SECRET },
 };

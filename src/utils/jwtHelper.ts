e01a0938508dac59f9f59ed671e2612f3acfc4dd
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-12-28 18:43:08 +0600

    [Update] EncodeToken Function

--- src/utils/jwtHelper.ts
+++ src/utils/jwtHelper.ts
@@ -6,8 +6,8 @@ import { Request } from "express";
 
 const encodeToken = (payload: IUser) => {
     const secret = config.jwt.secret as Secret;
-    const { _id, name, email, phone, image, role, isVerify } = payload;
-    return jwt.sign({ id: _id, name, email, phone, image, role, isVerify }, secret, { expiresIn: "7d" });
+    const { _id, name, email, phone, image, role, address, isVerify } = payload;
+    return jwt.sign({ id: _id, name, email, phone, image, role, address, isVerify }, secret, { expiresIn: "7d" });
 };
 
 const decodeToken = async (req: Request): Promise<JwtPayload | null> => {
